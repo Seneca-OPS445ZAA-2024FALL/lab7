014707c6a4f84e147d9e3a5d2577408fec02d6da
Shermalyn Andon <izhameandon@gmail.com>
2024-11-24 22:46:25 -0500

    Completed lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: Shermalyn Andon - 160564217
+
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
     """
-    def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
+    def __init__(self, hour=12, minute=0, second=0):
+        """Constructor for time object""" 
         self.hour = hour
         self.minute = minute
         self.second = second
@@ -15,16 +16,31 @@ def format_time(t):
     return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
 
 def sum_times(t1, t2):
-    """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
-    sum.hour = t1.hour + t2.hour
-    sum.minute = t1.minute + t2.minute
-    sum.second = t1.second + t2.second
-    return sum
+    """Add two time objects and return the sum."""
+    sum_time = Time(0, 0, 0)
+    sum_time.hour = t1.hour + t2.hour
+    sum_time.minute = t1.minute + t2.minute
+    sum_time.second = t1.second + t2.second
+
+    # Carry over seconds to minutes
+    if sum_time.second >= 60:
+        sum_time.minute += sum_time.second // 60
+        sum_time.second %= 60
+    
+    # Carry over minutes to hours
+    if sum_time.minute >= 60:
+        sum_time.hour += sum_time.minute // 60
+        sum_time.minute %= 60
+    
+    # Optional: You could also check for hour overflow if needed
+    if sum_time.hour >= 24:
+        sum_time.hour %= 24
+
+    return sum_time
 
 def valid_time(t):
-    """check for the validity of the time object attributes:
-        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+    """Check for the validity of the time object attributes:
+       24 > hour >= 0, 60 > minute >= 0, 60 > second >= 0 """
     if t.hour < 0 or t.minute < 0 or t.second < 0:
         return False
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
